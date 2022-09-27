34f58cf31a6b9a96d0723a79a1bed7f255558689
Etheryen <lukasz.ciwoniuk@gmail.com>
2022-09-27 22:49:37 +0200

    remove useless if statement

--- background.js
+++ background.js
@@ -2,5 +2,5 @@ let enabled = true;
 
 chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ enabled });
-  if(enabled) console.log('Extention enabled %csuccessfully.', 'color: lime');
+  console.log('Extention enabled %csuccessfully.', 'color: lime');
 });
